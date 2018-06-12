a2d5a757557a55ff286099d7a8dc207cba747ece
Julien LEMARCHAND <jlemarchand@PHILO.INTRA>
2018-06-12 09:38:42 +0200

    creation de la page contact

--- contact.php
+++ contact.php
@@ -2,10 +2,10 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>Git</title>
+    <title>Contact</title>
 </head>
 <body>
-    <h1>Accueil</h1>
-    Utilisation de Git
+    <h1>Liste des contact</h1>
+    Un commentaire
 </body>
 </html>
